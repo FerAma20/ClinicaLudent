ca9bc78f76caee3b7bade5174ecda5fac29b0859
FerAma20 <ferama22@gmail.com>
2023-08-06 18:05:02 -0600

    modified sidebar router

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 
 
         <Routes>
-         
+        <Route path="/" element={<Index />} />
           <Route path="/Customers" element={<Clients />} />
           <Route path="/NextAppointment" element={<NextAppointment />} />
         </Routes>
